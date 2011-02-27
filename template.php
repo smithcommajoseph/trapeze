959e3c3a56ceabaabee40b0b23029850a852ad02
Joseph Smith <joseph@TechnicolorEnvy.local>
2011-02-27 11:38:17 -0500

    adding theme hook

--- template.php
+++ template.php
@@ -1,5 +1,16 @@
 <?php
 
+/**
+ * Implements hook_theme().
+ */
+function trapeze_theme(){
+    $items = array();
+    return $items;
+}
+
+/**
+ * Implements hook_preprocess_page().
+ */
 function trapeze_preprocess_page(&$vars){
     // Add body class for theme.
     $vars['attr']['class'] .= ' trapeze';
